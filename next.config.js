f7df25461fc0cdd17de5c1b3506e12642df8ea1d
Marella <marellarahhal20@gmail.com>
2023-05-22 11:27:18 +0300

    going online

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   env: {
     mapbox_key:
       "pk.eyJ1IjoibWFyZWxsYSIsImEiOiJjbGZ2Z2djODUwNzFlM2ZvNzVnYmx1aXRjIn0.STHXakMgCnQ_N-oi5zHO8g",
-    server_url:"https://wepay-ali-aldayoub.onrender.com"  
+    server_url:"https://wepay-ali-aldayoub.cyclic.app"  
   },
 };
 
