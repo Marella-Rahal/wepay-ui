68f6889a07875a23c5e31b2baab1942c4dd08aad
Marella <marellarahhal20@gmail.com>
2023-05-22 06:41:36 +0300

    Some Changes in the structure

--- wepay-api/routes/store.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const storeController = require('../controllers/store');
-const authMiddleware = require('../middleware/authMiddleware');
-
-router.get('/getAllStores', authMiddleware.authenticateUser, storeController.getAllStores);
-
-module.exports = router;
